8ca8fc242a8455560c0cac3f40ebd9b21fbd2474
Vitor Buzinaro <vitor@fasterfront.com>
2023-11-26 16:16:24 -0300

    add permanent redirects - set HSTS header for preloading always-https at the browser level - upgrade http to https - www to non-www - remove trailing slash - remove double slashes

--- server.mjs
+++ server.mjs
@@ -12,6 +12,7 @@ const {
 const vite = await createViteServer?.()
 
 const app = express()
+app.use(permanentRedirects)
 
 if (vite) {
   app.use(vite.middlewares)
@@ -36,3 +37,37 @@ const port = process.env.PORT ?? 3000
 app.listen(port, '0.0.0.0', () =>
   console.log('Server started on http://localhost:' + port),
 )
+
+// --
+function permanentRedirects(req, res, next) {
+  let { path, url, headers } = req
+  let { host, 'x-forwarded-proto': protocol } = headers
+
+  if (protocol === 'https') {
+    res.set(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains; preload',
+    )
+  }
+
+  if (
+    (protocol !== 'http' || host.startsWith('localhost')) && // upgrade to https
+    !host.startsWith('www.') && // redirect www to non-www
+    (!path.endsWith('/') || path.length === 1) && // remove trailing slash
+    !path.includes('//') // remove double slashes
+  ) {
+    return next()
+  }
+
+  if (protocol === 'http' && !host.startsWith('localhost')) {
+    protocol = 'https'
+    res.set('X-Forwarded-Proto', 'https')
+  }
+  if (host.startsWith('www.')) {
+    host = host.slice(4)
+  }
+
+  const query = url.slice(path.length)
+  const safepath = path.slice(0, -1).replace(/\/+/g, '/')
+  res.redirect(308, `${protocol}://${host}${safepath}${query}`)
+}
